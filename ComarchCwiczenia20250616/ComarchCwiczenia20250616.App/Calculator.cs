7acc1da791123d67b3abf1c72f5a1c44440c2e9b
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2025-06-17 13:49:52 +0200

    Sprzątanie i zapętlenie programu

---
+++ ComarchCwiczenia20250616/ComarchCwiczenia20250616.App/Calculator.cs
@@ -0,0 +1,88 @@
+﻿namespace ComarchCwiczenia20250616.App;
+
+internal class Calculator
+{
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+
+    public int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+
+    public float Divide(float x, float y)
+    {
+        return x / y;
+    }
+
+    public int Modulo(int x, int y)
+    {
+        return x % y;
+    }
+
+    public void BubbleSort(int[] tab)
+    {
+        int n = tab.Length; // Długość tablicy
+
+        // Zewnętrzna pętla powtarza się n-1 razy
+        // Przy każdym przebiegu największa liczba "wędruje" na koniec
+        for (int i = 0; i < n - 1; i++)
+        {
+            // Wewnętrzna pętla porównuje sąsiednie elementy
+            // Nie musimy już sprawdzać ostatnich i elementów, które są już posortowane
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                // Sprawdzamy, czy element po lewej stronie jest większy od tego po prawej
+                if (tab[j] > tab[j + 1])
+                {
+                    // Jeśli tak, to je zamieniamy miejscami
+
+                    // Przechowujemy tymczasowo wartość z lewej strony
+                    int temp = tab[j];
+
+                    // Przypisujemy mniejszy element z prawej na miejsce większego
+                    tab[j] = tab[j + 1];
+
+                    // Przypisujemy większy element (z temp) po prawej stronie
+                    tab[j + 1] = temp;
+                }
+                // Jeśli elementy są już w dobrej kolejności, nic nie robimy
+            }
+            // Po każdej iteracji największy element "wypływa" na koniec tablicy
+        }
+        // Gdy zakończymy pętle, tablica jest posortowana rosnąco
+    }
+
+    public string CaesarsCipher(string? plainText, int shift)
+    {
+        string result = "";
+
+        foreach (char type in plainText)
+        {
+            if (type >= 'a' && type <= 'z')
+            {
+                char zaszyfrowany = (char)((type - 'a' + shift + 26) % 26 + 'a');
+                result += zaszyfrowany;
+            }
+            else if (type >= 'A' && type <= 'Z')
+            {
+                char zaszyfrowany = (char)((type - 'A' + shift + 26) % 26 + 'A');
+                result += zaszyfrowany;
+            }
+            else
+            {
+                result += type;
+            }
+        }
+
+        return result;
+    }
+
+}
--- ComarchCwiczenia20250616/ComarchCwiczenia20250616.App/Program.cs
+++ ComarchCwiczenia20250616/ComarchCwiczenia20250616.App/Program.cs
@@ -1,5 +1,4 @@
-﻿
-namespace ComarchCwiczenia20250616.App
+﻿namespace ComarchCwiczenia20250616.App
 {
     internal class Program
     {
@@ -10,65 +9,80 @@ namespace ComarchCwiczenia20250616.App
         /// <param name="args">parametry wejściowe aplikacji</param>
         public static void Main(string[] args)
         {
-            ShowMenu();
+            bool czyKontynuowac = false;
 
-            string sWybor = Console.ReadLine();
-            bool czyPoprawnyWybor = int.TryParse(sWybor, out int wybor);
-
-            if (czyPoprawnyWybor)
+            do
             {
-                int x, y;
+                ShowMenu();
 
-                switch (wybor)
+                string sWybor = Console.ReadLine();
+                bool czyPoprawnyWybor = int.TryParse(sWybor, out int wybor);
+
+                if (czyPoprawnyWybor)
                 {
-                    case 1:
-                        GetXY(out x, out y);
-                        Console.WriteLine($"Wynik dodawania {x} oraz {y} to {x + y}.");
-                        break;
-                    case 2:
-                        GetXY(out x, out y);
-                        Console.WriteLine($"Wynik odejmowania {x} oraz {y} to {x - y}.");
-                        break;
-                    case 3:
-                        GetXY(out x, out y);
-                        Console.WriteLine($"Wynik mnożenia {x} oraz {y} to {x * y}.");
-                        break;
-                    case 4:
-                        GetXY(out x, out y);
-                        Console.WriteLine($"Wynik dzielenia {x} oraz {y} to {x / (float)y}.");
-                        break;
-                    case 5:
-                        GetXY(out x, out y);
-                        Console.WriteLine($"Wynik reszty z dzielenia {x} oraz {y} to {x % y}.");
-                        break;
-                    case 6:
-                        Console.Write("Podaj tekst do zaszyfrowania: ");
-                        string jawnyTekst = Console.ReadLine();
-                        string zaszyfrowanyTekst = SzyfrCezara(jawnyTekst, 3);
-                        Console.WriteLine($"Zaszyfrowany tekst: {zaszyfrowanyTekst}");
-                        break;
-                    case 7:
-                        Console.Write("Podaj dowolną ilość liczb rozdzielonych spacją: ");
-                        string[] sLiczby = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                        int[] liczby = new int[sLiczby.Length];
-                        for (int i = 0; i < sLiczby.Length; i++)
-                        {
-                            liczby[i] = int.Parse(sLiczby[i]);
-                        }
+                    Console.Clear();
+                    Calculator calculator = new Calculator();
+                    int x, y;
 
-                        SortowanieBabelkowe(liczby);
-                        WyswietlTablice(liczby);
+                    switch (wybor)
+                    {
+                        case 1:
+                            GetXY(out x, out y);
+                            Console.WriteLine($"Wynik dodawania {x} oraz {y} to {calculator.Add(x, y)}.");
+                            break;
+                        case 2:
+                            GetXY(out x, out y);
+                            Console.WriteLine($"Wynik odejmowania {x} oraz {y} to {calculator.Subtract(x, y)}.");
+                            break;
+                        case 3:
+                            GetXY(out x, out y);
+                            Console.WriteLine($"Wynik mnożenia {x} oraz {y} to {calculator.Multiply(x, y)}.");
+                            break;
+                        case 4:
+                            GetXY(out x, out y);
+                            Console.WriteLine($"Wynik dzielenia {x} oraz {y} to {calculator.Divide(x, y)}.");
+                            break;
+                        case 5:
+                            GetXY(out x, out y);
+                            Console.WriteLine($"Wynik reszty z dzielenia {x} oraz {y} to {calculator.Modulo(x, y)}.");
+                            break;
+                        case 6:
+                            Console.Write("Podaj tekst do zaszyfrowania: ");
+                            string jawnyTekst = Console.ReadLine();
+                            string zaszyfrowanyTekst = calculator.CaesarsCipher(jawnyTekst, 3);
+                            Console.WriteLine($"Zaszyfrowany tekst: {zaszyfrowanyTekst}");
+                            break;
+                        case 7:
+                            Console.Write("Podaj dowolną ilość liczb rozdzielonych spacją: ");
+                            string[] sLiczby = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                            int[] liczby = new int[sLiczby.Length];
+                            for (int i = 0; i < sLiczby.Length; i++)
+                            {
+                                liczby[i] = int.Parse(sLiczby[i]);
+                            }
 
-                        break;
-                    default:
-                        ShowError("Podana wartość wykracza poza menu!");
-                        break;
+                            calculator.BubbleSort(liczby);
+                            WyswietlTablice(liczby);
+
+                            break;
+                        default:
+                            ShowError("Podana wartość wykracza poza menu!");
+                            break;
+                    }
                 }
-            }
-            else
-            {
-                ShowError("Podana wartość nie jest liczbą!");
-            }
+                else
+                {
+                    ShowError("Podana wartość nie jest liczbą!");
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+
+                Console.Write("Czy chcesz wykonać kolejną operację? [T|n]");
+                var key = Console.ReadKey().Key;
+                czyKontynuowac = key != ConsoleKey.N;
+
+            } while (czyKontynuowac);
         }
 
         private static void GetXY(out int x, out int y)
@@ -81,6 +95,8 @@ namespace ComarchCwiczenia20250616.App
 
         private static void ShowMenu()
         {
+            Console.Clear();
+
             Console.WriteLine("Kalkulator v1.0");
             Console.WriteLine(" 1. Dodawnie");
             Console.WriteLine(" 2. Odejmowanie");
@@ -106,64 +122,6 @@ namespace ComarchCwiczenia20250616.App
                 Console.Write(liczba + " ");
             }
             Console.WriteLine();
-        }
-
-        private static void SortowanieBabelkowe(int[] tablica)
-        {
-            int n = tablica.Length; // Długość tablicy
-
-            // Zewnętrzna pętla powtarza się n-1 razy
-            // Przy każdym przebiegu największa liczba "wędruje" na koniec
-            for (int i = 0; i < n - 1; i++)
-            {
-                // Wewnętrzna pętla porównuje sąsiednie elementy
-                // Nie musimy już sprawdzać ostatnich i elementów, które są już posortowane
-                for (int j = 0; j < n - 1 - i; j++)
-                {
-                    // Sprawdzamy, czy element po lewej stronie jest większy od tego po prawej
-                    if (tablica[j] > tablica[j + 1])
-                    {
-                        // Jeśli tak, to je zamieniamy miejscami
-
-                        // Przechowujemy tymczasowo wartość z lewej strony
-                        int temp = tablica[j];
-
-                        // Przypisujemy mniejszy element z prawej na miejsce większego
-                        tablica[j] = tablica[j + 1];
-
-                        // Przypisujemy większy element (z temp) po prawej stronie
-                        tablica[j + 1] = temp;
-                    }
-                    // Jeśli elementy są już w dobrej kolejności, nic nie robimy
-                }
-                // Po każdej iteracji największy element "wypływa" na koniec tablicy
-            }
-            // Gdy zakończymy pętle, tablica jest posortowana rosnąco
-        }
-
-        private static string SzyfrCezara(string? jawnyTekst, int przesuniecie)
-        {
-            string wynik = "";
-
-            foreach (char znak in jawnyTekst)
-            {
-                if (znak >= 'a' && znak <= 'z')
-                {
-                    char zaszyfrowany = (char)((znak - 'a' + przesuniecie + 26) % 26 + 'a');
-                    wynik += zaszyfrowany;
-                }
-                else if (znak >= 'A' && znak <= 'Z')
-                {
-                    char zaszyfrowany = (char)((znak - 'A' + przesuniecie + 26) % 26 + 'A');
-                    wynik += zaszyfrowany;
-                }
-                else
-                {
-                    wynik += znak;
-                }
-            }
-
-            return wynik;
         }
     }
 }
